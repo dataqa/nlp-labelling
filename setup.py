2060cb6ab5ca33fb7b0e9efd0451d982359afb52
DataQA <contact@dataqa.ai>
2021-10-24 13:50:38 +0100

    version bump

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ extra_files.extend(["api/static/bundle.js",
 
 setuptools.setup(
     name="dataqa",
-    version="1.0.9",
+    version="1.1.0",
     author="DataQA.AI",
     author_email="contact@dataqa.ai",
     description="Package to search and label documents",
