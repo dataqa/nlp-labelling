f6a0a13e7a4464dbde170e051ffdcbbc5c548bf6
DataQA <contact@dataqa.ai>
2021-11-25 14:13:47 +0000

    remove spacy download

--- src/dataqa/nlp/spacy_nlp.py
+++ src/dataqa/nlp/spacy_nlp.py
@@ -1,17 +1,6 @@
-from pathlib import Path
-import subprocess
 import sys
 
-import spacy
 import spacy.cli
-
-from dataqa.constants import ROOT_PATH
-
-try:
-    _ = spacy.cli.info('en-core-web-sm')
-except:
-    subprocess.check_call([sys.executable, "-m", "pip", "install",
-                           str(Path(ROOT_PATH, "nlp", "en_core_web_sm-2.3.1.tar.gz"))])
 
 # this is a pointer to the module object instance itself.
 this = sys.modules[__name__]
